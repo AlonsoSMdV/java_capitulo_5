37b2454ba59801a92e9716881d8668228bf8b52c
AlonsoSMdV <asanmor011004@gmail.com>
2022-10-18 12:11:34 +0200

    New commit

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,10 +1,4 @@
-/**
-* 5. Bucles
-*
-* 1. Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
-*
-* 
-*/
+
 public class Ejercicio1 {
     public static void main(String[] args) {
     for(int i = 0; i <= 100; i += 5) {
